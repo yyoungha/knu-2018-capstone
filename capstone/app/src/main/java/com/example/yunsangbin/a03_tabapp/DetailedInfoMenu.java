c85dfd29673c216915404d65a72fbf8e1349c864
wwolny <wwolny1@gmail.com>
2018-11-06 16:33:54 +0900

    working DetailedView

--- capstone/app/src/main/java/com/example/yunsangbin/a03_tabapp/DetailedInfoMenu.java
+++ capstone/app/src/main/java/com/example/yunsangbin/a03_tabapp/DetailedInfoMenu.java
@@ -1,9 +1,17 @@
 package com.example.yunsangbin.a03_tabapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +28,14 @@ public class DetailedInfoMenu extends AppCompatActivity {
 
         //TODO: change to JSON file pull data
 
+        ImageView ivPhoto = (ImageView)findViewById(R.id.ivIconDet);
+        TextView tvDescription = (TextView)findViewById(R.id.tvDescriptionDet);
+        TextView tvName = (TextView)findViewById(R.id.tvNameDet);
+        TextView tvKakaoId = (TextView)findViewById(R.id.tvKakaoDet);
 
-        lstData =new ArrayList<>();
-        lstData.add(new DataItem(R.drawable.picasso, "Picasso was a great artist", "Pablo Picasso", "picasso123"));
-        CustomAdapter adapter = new CustomAdapter(this, R.layout.activity_detailed_info_menu, lstData);
-
-
+        tvDescription.setText("Picasso was a great artist");
+        tvName.setText("Pablo Picasso");
+        ivPhoto.setImageResource(R.drawable.picasso);
+        tvKakaoId.setText("picasso123");
     }
-
 }
