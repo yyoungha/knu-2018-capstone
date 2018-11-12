a47fd3cb0746c1a6ba29c11e57c990d79ae58462
makeastir <hkm_m@naver.com>
2018-11-13 03:12:08 +0900

    upload result of conection success with MySQL

---
+++ DB/11.13_Mysql Connection Test/workspace/src/main/java/com/codingsquid/spring4Tutorial/model/Student.java
@@ -0,0 +1,13 @@
+package com.codingsquid.spring4Tutorial.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private int identifierNum;
+
+}
