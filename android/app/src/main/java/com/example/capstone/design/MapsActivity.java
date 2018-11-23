a6ca37cf48b7eb91d8a2b5461442f7e5cf84f204
Young <zwei2@naver.com>
2018-11-23 23:55:42 +0900

    add design

--- android/app/src/main/java/com/example/capstone/design/MapsActivity.java
+++ android/app/src/main/java/com/example/capstone/design/MapsActivity.java
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+// 커스텀 http://gun0912.tistory.com/57
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
