c01fce6edbc631fbad6ccabbf91e649495187539
bobmukza <kor135@naver.com>
2018-11-23 09:02:05 +0900

    marke_test
    market 부분 recyclerview 아이템 여러개 들어가는 거 테스트 했음 db연결만 해주면 완료

--- android/app/src/main/java/com/example/capstone/design/ItemInfo.java
+++ android/app/src/main/java/com/example/capstone/design/ItemInfo.java
@@ -3,10 +3,11 @@ package com.example.capstone.design;
 public class ItemInfo { //trade 할때 필요한 사진과 글 내용
 
     public int drawableId;
-    public String text;
-        public ItemInfo(int drawableId,String text){
+    public String txt1,date;
+        public ItemInfo(int drawableId,String txt1,String txt2){
             this.drawableId = drawableId;
-            this.text = text;
+            this.txt1 = txt1;
+            this.date = txt2;
         }
 }
 
--- android/app/src/main/java/com/example/capstone/design/Itemenroll.java
+++ android/app/src/main/java/com/example/capstone/design/Itemenroll.java
@@ -4,8 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Itemenroll extends AppCompatActivity {
+
+    private EditText txt1,txt2; //제목 내용
+    public String msg;
+
+    private Button btn;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +31,19 @@ public class Itemenroll extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        txt1 = (EditText)findViewById(R.id.Title);
+        msg = txt1.getText().toString();
+
+
+        btn = (Button)findViewById(R.id.complete);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("Electronics").push().setValue(msg);  //child는 JSON 데이터 형식에서 데이터의 '이름'을 의미
+                databaseReference.child("Electronics").child("gbgg").setValue(msg); //message-gbgg항목의 값을 msg로 덮어 씌운다 문약 -gbgg항목이 없으면 새로 생
+            }
+        });
 
     }
 
